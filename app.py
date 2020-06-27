454ded00b6e174acad47f2986f9aad61fef375f0
Rjumko <rjumko@gmail.com>
2020-06-27 12:22:44 +0700

    Auto deploy heroku

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 @app.route("/")
 def generate_buzz():
     page = '<html><body><h1>'
+    page += 'ABCDDDD '
     page += generator.generate_buzz()
     page += '</h1><body></html>'
     return page
